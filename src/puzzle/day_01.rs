28e89c79f47cb1ecdda81b3ffd2f7c3370ba6529
Yves Bonami <yves.bonami@gmail.com>
2022-12-01 09:34:56 +0100

    Solve Day One

--- src/puzzle/day_01.rs
+++ src/puzzle/day_01.rs
@@ -2,6 +2,7 @@
 
 use std::fmt::Display;
 
+/// --- Day 1: Calorie Counting ---
 pub struct DayOne {
     input: &'static str,
 }
@@ -12,11 +13,62 @@ impl crate::Puzzle for DayOne {
     }
 
     fn part_one(&self) -> String {
-        todo!()
+        let split = DayOne::split_input(self.input);
+
+        let mut i = 0;
+        let mut result = 0;
+
+        //  Loop over elves
+        while i < split.len() {
+            // Get the next elf
+            let elf: Vec<u32> = split[i..split.len()]
+                .iter()
+                .map_while(|x| x.parse::<u32>().ok())
+                .collect();
+            i += elf.len() + 1;
+
+            // Get the calories
+            let sum: u32 = elf.iter().sum();
+            if sum > result {
+                result = sum;
+            }
+        }
+
+        result.to_string()
     }
 
     fn part_two(&self) -> String {
-        todo!()
+        let split = DayOne::split_input(self.input);
+
+        let mut i = 0;
+        let mut result: Vec<u32> = vec![];
+
+        // Loop over elves
+        while i < split.len() {
+            // Get the next elf
+            let elf: Vec<u32> = split[i..split.len()]
+                .iter()
+                .map_while(|x| x.parse::<u32>().ok())
+                .collect();
+            i += elf.len() + 1;
+            result.push(elf.iter().sum())
+        }
+
+        // Sort by most calories
+        result.sort_by(|a, b| b.cmp(a));
+
+        // Return the sum of the top 3
+        result.iter().take(3).sum::<u32>().to_string()
+    }
+}
+
+impl DayOne {
+    fn split_input(input: &str) -> Vec<&str> {
+        input
+            .split_terminator("\n")
+            .into_iter()
+            .map(|x| x.trim())
+            .collect::<Vec<&str>>()
     }
 }
 
